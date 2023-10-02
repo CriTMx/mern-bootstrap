b60ff6e24a94d4f932bfe2c44058a6f062e44944
CriTMx <savefailedmc@gmail.com>
2023-10-02 19:06:49 +0530

    add task 7, 8

---
+++ JavaScript/script7.js
@@ -0,0 +1,11 @@
+function startGIF()
+{
+    var GIFHolder = document.getElementById('imgSource');
+    GIFHolder.src = 'rubiks2x.gif';
+}
+
+function stopGIF()
+{
+    var GIFHolder = document.getElementById('imgSource');
+    GIFHolder.src = 'rubiks2x_stopped.png';
+}
